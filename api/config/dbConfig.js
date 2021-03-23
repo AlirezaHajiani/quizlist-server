c11f1d255918fe086bf60af8c11a30c6e2d9339f
Alireza Hajiani <81192146+AlirezaHajiani@users.noreply.github.com>
2021-03-23 08:43:52 +0430

    Update dbConfig.js

--- api/config/dbConfig.js
+++ api/config/dbConfig.js
@@ -2,7 +2,7 @@
 module.exports = {
   HOST: "localhost",
   PORT: 27017,
-  DB: "quizlist_main",
-  USR: "quizlist_user",
-  PASS: encodeURIComponent("8k9958523A"),
+  DB: "",
+  USR: "",
+  PASS: encodeURIComponent(""),
 };
